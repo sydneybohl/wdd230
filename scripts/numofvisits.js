e259de7fe1fce952c7f99abd80196a8ad75d599a
sydneybohl <boh23006@byui.edu>
2024-02-22 07:10:45 -0600

    homepage updated

---
+++ scripts/numofvisits.js
@@ -0,0 +1,18 @@
+// Initialize the display element variable. 
+const visitsDisplay = document.querySelector(".visits");
+
+// Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+// Determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+// Increment the number of visits by one.
+numVisits++;
+
+// Store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
